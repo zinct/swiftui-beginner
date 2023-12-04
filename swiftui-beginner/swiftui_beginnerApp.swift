a2df647c0b258eaf307539f00864381d88c50b93
Indra Mahesa <indramahesa128@gmail.com>
2023-12-04 21:03:00 +0700

    Initial Commit

---
+++ swiftui-beginner/swiftui_beginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_beginnerApp.swift
+//  swiftui-beginner
+//
+//  Created by Indra Mahesa on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_beginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
